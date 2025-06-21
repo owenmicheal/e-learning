bb18abd97ae291554518c9708b4d1cf9544efd86
Kimbowa Owen <owenmicheal3rd@gmail.com>
2025-06-21 14:02:25 +0300

    fixed shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
 const Page = () => {
   return (
-    <div>Page</div>
-  )
-}
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-2xl underline text-red-600">
+        Welcome To Najji Teach
+      </h1>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
