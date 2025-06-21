06f813a538535e931146b2c0e3aa5000bb79effe
Kimbowa Owen <owenmicheal3rd@gmail.com>
2025-06-21 19:29:43 +0300

    implement companion card component styles and promps

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,30 @@ const Page = () => {
       <h1>Popular Companions</h1>
 
       <section className="home-section">
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
+        <CompanionCard
+          id="123"
+          name="Introduction to JavaScript Language"
+          topic="Learn the basics of JavaScript."
+          subject="Programming"
+          duration={45}
+          color="#97CBFB"
+        />
+        <CompanionCard
+          id="456"
+          name="Introduction to Mechanical Engineering"
+          topic="Explore the fundamentals of mechanical engineering."
+          subject="Engineering"
+          duration={25}
+          color="#C2E1FD"
+        />
+        <CompanionCard
+          id="234"
+          name="Introduction to Vector Illustration"
+          topic="Master the art of vector illustration."
+          subject="Design"
+          duration={48}
+          color="#FFC2D9"
+        />
       </section>
 
       <section className="home-section">
--- components/cards/CompanionCard.tsx
+++ components/cards/CompanionCard.tsx
@@ -1,5 +1,58 @@
-const CompanionCard = () => {
-  return <div>CompanionCard</div>;
+import Image from "next/image";
+import Link from "next/link";
+
+interface CompanionCardProps {
+  id: string;
+  name: string;
+  subject: string;
+  topic: string;
+  color: string;
+  duration: number;
+}
+
+const CompanionCard = ({
+  id,
+  name,
+  subject,
+  topic,
+  color,
+  duration,
+}: CompanionCardProps) => {
+  return (
+    <article className="companion-card" style={{ backgroundColor: color }}>
+      <div className="flex justify-between items-center">
+        <div className="subject-badge">{subject}</div>
+        <button className="companion-bookmark">
+          <Image
+            src="/icons/bookmark.svg"
+            alt="Bookmark Icon"
+            width={12.5}
+            height={15}
+          />
+        </button>
+      </div>
+
+      <h2 className="text-2xl font-bold">{name}</h2>
+      <p className="text-sm">{topic}</p>
+
+      <div className="flex items-center gap-2">
+        <Image
+          src="/icons/clock.svg"
+          alt="Duration"
+          width={13.5}
+          height={13.5}
+        />
+
+        <p className="text-sm">{duration} minutes</p>
+      </div>
+
+      <Link href={`/companions/${id}`} className="w-full">
+        <button className="btn-primary w-full justify-center">
+          Start Learning
+        </button>
+      </Link>
+    </article>
+  );
 };
 
 export default CompanionCard;
